e744968945acc4cf165ff169a7cc6ec26cf0ac42
RudzaniMatidze <rdzmatidze@gmail.com>
2025-01-28 19:52:03 +0200

    Script to execute the main file

---
+++ miniblog.py
@@ -0,0 +1,8 @@
+"""
+Before running the first version of your application, Flask needs to be told how
+to import it  by setting the FLASK_APP environment variable:
+`(venv) $ set FLASK_APP=miniblog.py` // Microsoft Windows
+`(venv) $ export FLASK_APP=miniblog` // Linux or Mac OS
+Then run your first application:
+`(venv) $ flask run` // view your application on local browser
+ """
