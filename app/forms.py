dabc0693b21df4d6130b0b18c09fd5a053ff51a5
RudzaniMatidze <rdzmatidze@gmail.com>
2025-02-03 01:47:01 +0200

    Update: added Registration form

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,40 @@
 """ Module to store the web classes """
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
+
+# Create user Login form
 class LoginForm(FlaskForm):
     """ Create an object for each field as a class variable """
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+# Create user Registration form
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = StringField('Password', validators=[DataRequired()])
+    password2 = StringField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    # Logic to validate user's username during registration
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(
+            User.username == username.data))
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    # Logic to validate user's email during registration
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User). where(
+            User.email == email.data))
+        if user is not None:
+            raise ValidationError('Please use a different email')
