2df67b105519c834fb06fe046902b5ed2649be4d
RudzaniMatidze <rdzmatidze@gmail.com>
2025-02-03 21:50:57 +0200

    Update: added user registration route

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 """ Routes script handles the different URLs that the application supports"""
 
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from urllib.parse import urlsplit
-from app.forms import LoginForm
+from flask_login import logout_user, login_required, current_user, login_user
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
-from sqlalchemy import sa
+import sqlalchemy as sa
 from app import app, db
 
 # Create an index route
@@ -40,11 +41,30 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign in', form=form)
 
-# create logout route
+# Create logout route
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+# Create a Register route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Registration view function logic
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congradulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
