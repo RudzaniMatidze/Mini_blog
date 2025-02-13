5e9549cb8bb02e97ef960bb401ff6e87a3aea988
RudzaniMatidze <rdzmatidze@gmail.com>
2025-02-14 01:27:21 +0200

    Update: add email logic

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler
+import os
 
 
 app = Flask(__name__)
@@ -32,5 +33,7 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+        # add a logger file here
+    
 
 from app import routes, models, errors
